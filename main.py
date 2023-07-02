630694ebe8f5832bd19e54198e1a573361af42c2
Shu Sian Lyu <shusianlyu@gmail.com>
2023-07-01 20:10:50 -0700

    Send the screenshots via email

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ while True:
 
     # Object exit the frame
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
