d17d4511209c89f774378f4c3cce2b36f85b4c50
Shu Sian Lyu <shusianlyu@gmail.com>
2023-07-01 19:58:36 -0700

    Save the images of the object

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import time
 import cv2
 from emailing import send_email
@@ -7,6 +8,8 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
@@ -31,6 +34,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
